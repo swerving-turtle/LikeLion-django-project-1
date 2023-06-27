364a26cab5b150f12b18093eb45a7a459e0b7702
Keonwoo Moon <swervingturtle@gmail.com>
2023-06-27 10:05:31 +0900

    home, base html 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,7 +59,11 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+            'blog/templates/blog',
+            'bookmark/templates/bookmark',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from bookmark.views import BookmarkLV, BookmarkDV
+from mysite.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookmark/', include('bookmark.urls')),
     # path('bookmark/', BookmarkLV.as_view(), name='index'),
     # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
+    path('', HomeView.as_view(), name='home'),
     path('blog/', include('blog.urls')),
 ]
