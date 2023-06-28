9e79c757966de629e60bdbfcf395da53aff7e13e
Keonwoo Moon <swervingturtle@gmail.com>
2023-06-28 11:41:51 +0900

    photo app 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'photo.apps.PhotoConfig',
     'blog.apps.BlogConfig',
     'bookmark.apps.BookmarkConfig',
     'django.contrib.admin',
