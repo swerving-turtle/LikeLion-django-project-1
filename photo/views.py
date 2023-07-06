a8ac44a78b64f3b0c113e619e7ad1bcdf8ac3434
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-06 10:13:50 +0900

    photo view 로직 추가

--- photo/views.py
+++ photo/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from mysite.views import OwnerOnlyMixin
 from photo.models import Album, Photo
 
 
@@ -13,3 +16,28 @@ class AlbumDV(DetailView):
 
 class PhotoDV(DetailView):
     model = Photo
+
+class PhotoCV(LoginRequiredMixin, CreateView):
+    model = Photo
+    fields = ('album', 'title', 'image', 'description')
+    success_url = reverse_lazy('photo:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class PhotoChangeLV(LoginRequiredMixin, ListView):
+    model = Photo
+    template_name = 'photo/photo_change_list.html'
+
+    def get_queryset(self):
+        return Photo.objects.filter(owner=self.request.user)
+
+class PhotoUV(OwnerOnlyMixin, UpdateView):
+    model = Photo
+    fields = ('album', 'title', 'image', 'description')
+    success_url = reverse_lazy('photo:index')
+
+class PhotoDelV(OwnerOnlyMixin, DeleteView):
+    model = Photo
+    success_url = reverse_lazy('photo:index')
