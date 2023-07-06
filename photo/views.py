1bd0f7e6fa8a3e788b43b2bb956efa496a10e195
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-06 10:34:08 +0900

    photo view 로직 추가

--- photo/views.py
+++ photo/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from mysite.views import OwnerOnlyMixin
+from photo.forms import PhotoInlineFormSet
 from photo.models import Album, Photo
 
 
@@ -41,3 +42,62 @@ class PhotoUV(OwnerOnlyMixin, UpdateView):
 class PhotoDelV(OwnerOnlyMixin, DeleteView):
     model = Photo
     success_url = reverse_lazy('photo:index')
+
+class AlbumChangeLV(LoginRequiredMixin, ListView):
+    model = Album
+    template_name = 'pohto/album_change_list.html'
+    def get_queryset(self):
+        return Album.objects.filter(owner=self.request.user)
+
+class AlbumDelV(OwnerOnlyMixin, DeleteView):
+    model = Album
+    success_url = reverse_lazy('photo:index')
+
+class AlbumPhotoCV(LoginRequiredMixin, CreateView):
+    model = Album
+    fields = ('name', 'description')
+    success_url = reverse_lazy('photo:index')
+
+    def get_context_date(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES)
+        else:
+            context['formset'] = PhotoInlineFormSet()
+        return context
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        context = self.get_context_data()
+        formset = context['formset']
+        for photoform in formset:
+            photoform.instance.owner = self.request.user
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.render_to_response(self.get_context_date(form=form))
+
+class AlbumPhotoUV(OwnerOnlyMixin, UpdateView):
+    model = Album
+    fields = ('name', 'description')
+    success_url = reverse_lazy('photo:index')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES, instance=self.object)
+        else:
+            context['formset'] = PhotoInlineFormSet(instance=self.object)
+        return context
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
